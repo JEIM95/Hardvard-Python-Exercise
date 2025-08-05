20842f1e234c57450b5ad8e95e10ee105f5888a3
Jose Enrique <jose_galicia_95@hotmail.com>
2025-08-05 15:31:55 +0200

    V1: Lectura-escritura de csv

---
+++ Final_Project/project.py
@@ -0,0 +1,71 @@
+import csv
+import sys
+from datetime import date
+
+"""
+    This project enables you to track and store your financial transactions in a CSV file. 
+    Based on your input, you can update your name, record income, expenses, and other financial activities.
+"""
+#Variables globales
+financials = []
+
+def main():
+    global financials
+
+    #Función para guarda CSV en una lista de diccionarios
+    Read_CSV()
+
+    for financial in financials:
+        print(f"fecha: {financial["fecha"]}, Concepto: {financial["concepto"]}, ingresos/gastos: {financial["ingresos/gastos"]}, balance: {financial["balance"]}")
+
+    concept = input("Introduce el concepto: ")
+    valor = input("Introduce ingresos/gastos: ")
+    #Hacer función de control de datos
+    Write_CSV(concept, valor)
+
+def Read_CSV():
+    global financials
+    financials = []
+
+    with open("financial.csv", "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            financials.append({"fecha": row["fecha"], "concepto": row["concepto"], "ingresos/gastos": row["ingresos/gastos"], "balance": row["balance"]})
+  
+
+def Write_CSV(concept, n):
+    global financials
+    #Primero guardar CSV en lista de diccionarios
+    #Read_CSV()
+    
+    try:
+        ultimo_balance = financials[-1]["balance"]
+    except IndexError:
+        total=0
+    else:
+        total = int(ultimo_balance)
+
+    valor = int(n)  #n es ingresos/gastos
+
+    total = total + valor #Balance
+
+    date_today = date.today() #Fecha de hoy yyyy-mm-dd
+
+    financials.append(
+                {
+                    "fecha":date_today, 
+                    "concepto":concept, 
+                    "ingresos/gastos":valor, 
+                    "balance":total
+                }
+            )
+    with open("financial.csv", "w", newline = "") as file:
+        writer = csv.DictWriter(file, fieldnames=["fecha", "concepto", "ingresos/gastos", "balance"])
+        
+        writer.writeheader()
+        writer.writerows(financials)
+
+
+
+if __name__ == "__main__":
+    main()
